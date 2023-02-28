78513eaffd493b49f3b6423235f4b8ffa0fbd1ac
sveske_juice <carl.benjamin.dreyer@gmail.com>
2023-02-28 14:58:47 +0100

    rename parsed

--- expressions.h
+++ expressions.h
@@ -11,18 +11,18 @@ typedef exprtk::expression<T>   expression_t;
 typedef exprtk::parser<T>       parser_t;
 
 
-struct parsedExpression {
+struct expressionResult {
     bool isError;
     std::string message;
     T value;
 
-    parsedExpression(bool _isError,std::string _message) {
+    expressionResult(bool _isError,std::string _message) {
         value = 0;
         isError = _isError;
         message = _message;
     }
 
-    parsedExpression(T _value) {
+    expressionResult(T _value) {
         value = _value;
         isError = false;
         message = "";
@@ -36,7 +36,7 @@ public:
 
     static void initialize();
 
-    static parsedExpression parseExpression(std::string input);
+    static expressionResult parseExpression(std::string input);
 
     static std::string parseExpressionToString(std::string input);
 
