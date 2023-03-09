435256136b880ad63e5c2cf4ce6e69dc2cb48989
SpoodyTheOne <sxsgamer@gmail.com>
2023-03-09 10:38:10 +0100

    q

--- src/base/expression_parser/node.h
+++ src/base/expression_parser/node.h
@@ -15,11 +15,20 @@ public:
      * @return True if it's the deepest. False if not.
      */
     bool isDeepestOperandNode() const;
-    NodeType GetNodeType() const;
+
+    void replaceChild(Node *child, Node *newChild) {
+        auto left_ptr = m_LeftChild.get();
+        auto right_ptr = m_LeftChild.get();
+
+        if (left_ptr == child) {
+            left_ptr = newChild;
+        } else if ()
+    }
 
 private:
     std::unique_ptr<Node> m_LeftChild{};
     std::unique_ptr<Node> m_RightChild{};
+    std::unique_ptr<Node> m_ParentNode;
     NodeType m_NodeType;
     Number m_Value;
 };
