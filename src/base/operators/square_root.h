607eb5d6b287af8253ef57567da787112aecb7d6
SpoodyTheOne <sxsgamer@gmail.com>
2023-03-20 12:27:48 +0100

    sqrt.h

---
+++ src/base/operators/square_root.h
@@ -0,0 +1,29 @@
+#ifndef SQUARE_ROOT_H
+#define SQUARE_ROOT_H
+
+#include "operator.h"
+
+class Square_Root : public Operator
+{
+public:
+    inline static const std::string name = "Square Root";
+    inline static const std::string sign = "sqrt";
+    inline static const int predecence = Precedence::Parenthesis;
+    inline static const int argumentCount = 1;
+
+    std::string getName() { return name; }
+    std::string getSign() { return sign; }
+    int getPredecence() { return predecence; }
+    int getArgumentCount() { return argumentCount; }
+
+    static Operator* create() { return new Square_Root(); }
+
+    ExpressionValue doOperation(std::vector<ExpressionValue> args)
+    {
+        assert(args.size() == argumentCount);
+        return std::sqrt(args[0]);
+    }
+};
+
+
+#endif // SQUARE_ROOT_H
