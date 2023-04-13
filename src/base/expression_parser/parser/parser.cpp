445f8431d946f26cffb3c64359b860b048b1880c
SpoodyTheOne <sxsgamer@gmail.com>
2023-04-13 12:24:33 +0200

    Added private functions for grammar rules

--- src/base/expression_parser/parser/parser.cpp
+++ src/base/expression_parser/parser/parser.cpp
@@ -1,6 +1,46 @@
 #include "parser.h"
 
-Parser::Parser()
+Expression Parser::expression()
+{
+    return equality();
+}
+
+Expression Parser::equality()
+{
+
+}
+
+Expression Parser::comparison()
+{
+
+}
+
+Expression Parser::term()
+{
+
+}
+
+Expression Parser::factor()
+{
+
+}
+
+Expression Parser::unary()
+{
+
+}
+
+Expression Parser::exponent()
+{
+
+}
+
+Expression Parser::factorial()
+{
+
+}
+
+Expression Parser::primary()
 {
 
 }
--- src/base/expression_parser/parser/parser.h
+++ src/base/expression_parser/parser/parser.h
@@ -3,6 +3,7 @@
 
 
 #include "base/expression_parser/lexer/token.h"
+#include "base/expression_parser/parser/expression.h"
 #include <vector>
 class Parser
 {
@@ -13,6 +14,16 @@ public:
 private:
     std::vector<Token *> tokens;
     int currentToken = 0;
+
+    Expression expression();
+    Expression equality();
+    Expression comparison();
+    Expression term();
+    Expression factor();
+    Expression unary();
+    Expression exponent();
+    Expression factorial();
+    Expression primary();
 };
 
 #endif // PARSER_H
